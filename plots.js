ea268b37b6c4bb3bcb650dc8c682008c751cd2ae
ajawa-took <ajawa_took@yahoo.com>
2022-12-01 15:15:54 -0800

    small steps

--- plots.js
+++ plots.js
@@ -2,7 +2,7 @@
 function init() {
   var selector = d3.select("#selDataset");
 
-  var sampleNames = samples.names;
+  var sampleNames = data.names;
   sampleNames.forEach((sample) => {
     selector
       .append("option")
@@ -12,38 +12,13 @@ function init() {
 
 
 
-d3.selectAll("#dropdownMenu").on("change", updatePlotly);
-  
-  function updatePlotly() {
-    var dropdownMenu = d3.select("#dropdownMenu");
-    var dataset = dropdownMenu.property("value");
-  
-    var xData = [1, 2, 3, 4, 5];
-    var yData = [];
-  
-    if (dataset === 'dataset1') {
-      yData = [1, 2, 4, 8, 16];
-    };
-  
-    if (dataset === 'dataset2') {
-      yData = [1, 10, 100, 1000, 10000];
-    };
-  
-    var trace = {
-      x: [xData],
-      y: [yData],
-    };
-    Plotly.restyle("plot", trace);
-  };
-
-
-function optionChanged(newSample) {
+  function optionChanged(newSample) {
   buildMetadata(newSample);
   buildCharts(newSample);
 }
 
 function buildMetadata(sample) {
-    var metadata = samples.metadata;
+    var metadata = data.metadata;
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
     var PANEL = d3.select("#sample-metadata");
