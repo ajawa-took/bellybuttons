1a64b6e85f1891414434baa6015aad5f4ab68ae3
ajawa-took <ajawa_took@yahoo.com>
2022-12-01 22:05:22 -0800

    deli1 halfdone

--- plots.js
+++ plots.js
@@ -8,11 +8,13 @@ function init() {
       .append("option")
       .text(sample)
       .property("value", sample);
-  })};
+  });
+  optionChanged("940")
+  };
 
 
 
-  function optionChanged(newSample) {
+function optionChanged(newSample) {
   buildMetadata(newSample);
   buildCharts(newSample);
 }
@@ -28,6 +30,21 @@ function buildMetadata(sample) {
       PANEL.append("h6").text(pear[0]+": "+pear[1]+"\n"));
 }
 
+function buildCharts(sample) {
+  var samples = data.samples;
+  var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+  var result = resultArray[0];
+      // 8. Create the trace for the bar chart. 
+  
+  //    result.otu_ids, result.otu_labels, result.sample_values
 
+  var barData = [ {x: result.sample_values, y: result.otu_ids, 
+      text: result.otu_labels,
+      type: "bar", orientation: 'h'}];
+      // 9. Create the layout for the bar chart. 
+  var barLayout = {title : "Top 10 Bacteria Cultures Found"};
+      // 10. Use Plotly to plot the data with the layout. 
+  Plotly.newPlot("bar", barData, barLayout) ;
+}
 
 init();
